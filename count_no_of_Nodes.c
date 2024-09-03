2e7e2b5f964d975f6651275bd92536d367a7ea7a
SthuthiGeetha7Chilukuri <sthuthigeetha1@gmail.com>
2024-09-03 13:48:14 +0530

    Count number of nodes in single linked list

---
+++ count_no_of_Nodes.c
@@ -0,0 +1,46 @@
+/*Count number of nodes in single linked list*/
+
+#include<stdio.h>
+#include<stdlib.h>
+struct node
+{
+        int data;
+        struct node * next;
+};
+
+int main()
+{
+        struct node *head=NULL,*temp=NULL,*new_node=NULL;
+        int choice=1,cnt=0;
+        while(choice)
+        {
+
+        new_node=(struct node*)malloc(sizeof(struct node));
+        printf("Enter data\n");
+        scanf("%d",&new_node->data);
+	cnt++;
+        if(head==NULL)
+        {
+                head=temp=new_node;
+
+        }
+        else
+        {
+                temp->next=new_node;
+                temp=new_node;
+        }
+        printf("continue?\n");
+        scanf("%d",&choice);
+        }
+        temp=head;
+        while(temp!=NULL)
+        {
+                printf("%d ",temp->data);
+                printf("%p -> ",temp);
+                printf("0x%x  ",temp->next);
+                temp=temp->next;
+        }
+	printf("\n%d\n",cnt);
+}
+
+
